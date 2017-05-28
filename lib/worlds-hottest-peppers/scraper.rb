9ce6a0925d47ca01dafc3eda4489dc16696b4d04
GDS83192 <Haku#83192>
2017-05-27 21:58:38 -0400

    adding scraper and cli

---
+++ lib/worlds-hottest-peppers/scraper.rb
@@ -0,0 +1,16 @@
+class WorldsBestRestaurants::Scraper
+
+  def get_page
+    Nokogiri::HTML(open("https://www.cayennediane.com/big-list-of-hot-peppers/"))
+  end
+
+  def scrape_peppers_index
+    self.get_page.css("div#t1-50 li")
+  end
+
+  def make_peppers
+    scrape_peppers_index.each do |r|
+      WorldsHottestPeppers::Pepper.new_from_index_page(r)
+    end
+  end
+end
